910af60c81bc7c5f564e287e9038540ae6528120
neryaRez <neryarez@gmail.com>
2023-05-01 19:51:04 +0300

    frac

---
+++ sources/Farction.cpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Fraction.hpp"
+
+using namespace std;
+using namespace ariel;
+
+namespace ariel{
+
+    Fraction::Fraction(int num, int denom):
+    numerator(num), denominator(denom)
+    {
+        
+
+
+    }
+
+
+
+
+}
---
+++ sources/Fraction.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+
+using namespace std;
+using namespace ariel;
+
+namespace ariel{
+
+    class Fraction
+    {
+    private:
+        int& numerator;
+        int& denominator;
+    
+    public:
+        Fraction(int num, int denom);
+        
+    };
+    
+    
+    
+
+
+
+
+
+}
